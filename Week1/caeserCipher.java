4b58c0624470c8a54b68fdb16d3fa7ad3f9296ba
mohitj8790 <mohitj8790@gmail.com>
2020-06-05 23:29:26 +0530

    Encrypting and Decrypting the original message using key

--- Week1/caeserCipher.java
+++ Week1/caeserCipher.java
@@ -25,13 +25,35 @@ public class caeserCipher
         return encrypted.toString();
     }
 
+    public String decrypt(String message,int key)
+    {
+        StringBuilder decrypted=new StringBuilder(message);
+        String alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String shifted=alphabet.substring(key)+alphabet.substring(0,key);
+
+        for(int i=0;i<decrypted.length();i++)
+        {
+            char currentChar=decrypted.charAt(i);
+            int idx=alphabet.indexOf(currentChar);
+
+            if(idx!=-1)
+            {
+                char newChar=shifted.charAt(idx);
+                decrypted.setCharAt(i, newChar);
+            }
+        }
+
+        return decrypted.toString();
+    }
+
     public void testCaeser() {
         int key=17;
         FileResource fr=new FileResource();
         String message=fr.asString();
         String encrypted=encrypt(message,key);
         System.out.println("Encrypted: "+encrypted);
-        
+        String decrypted=decrypt(encrypted,26-key);
+        System.out.println("Decrypted: "+decrypted);
     }
 
     public static void main(String[] args) {
