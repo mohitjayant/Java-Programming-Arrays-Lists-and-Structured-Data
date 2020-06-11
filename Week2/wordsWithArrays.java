a41fdc7cd9387012d785a3f5292a0c1a36e3ff98
mohitj8790 <mohitj8790@gmail.com>
2020-06-11 20:31:27 +0530

    Using Storage resource

---
+++ Week2/wordsWithArrays.java
@@ -0,0 +1,23 @@
+package Week2;
+import edu.duke.*;
+
+public class wordsWithArrays 
+{
+    StorageResource myWords;
+
+    public wordsWithArrays()
+    {
+        myWords=new StorageResource();
+    }
+
+    public void tester() 
+    {
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        wordsWithArrays wwa=new wordsWithArrays();
+        wwa.tester();
+    }
+}
