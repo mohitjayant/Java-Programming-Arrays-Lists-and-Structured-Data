96325b4733b9101e6879b9e1a1c8d6c0525b1e39
mohitj8790 <mohitj8790@gmail.com>
2020-06-12 01:08:37 +0530

    Use StorageResource

--- Week2/wordsWithArrays.java
+++ Week2/wordsWithArrays.java
@@ -9,10 +9,49 @@ public class wordsWithArrays
     {
         myWords=new StorageResource();
     }
+    public void readFile() 
+    {
+        myWords.clear();
+        FileResource fr=new FileResource();
+        for(String s:fr.words())
+        {
+            myWords.add(s.toLowerCase());
+        }
+    }
+
+    public boolean contains(String[] list,String word,int number) 
+    {
+        for(int k=0;k<number;k++)
+        {
+            if(list[k].equals(word))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int numberOfUniqueWords() 
+    {
+        int numStored=0;
+        String[] words=new String[myWords.size()];
+        for(String s:myWords.data())
+        {
+            if(!contains(words,s,numStored))
+            {
+                words[numStored]=s;
+                numStored++;
+            }
+        }
+        return numStored;
+    }
 
     public void tester() 
     {
-        
+        readFile();
+        System.out.println("Number of words in file: "+myWords.size());
+        int unique=numberOfUniqueWords();
+        System.out.println("Unique Words: "+unique);
     }
 
     public static void main(String[] args) 
