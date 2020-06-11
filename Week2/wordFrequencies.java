6bb3bc6c12edb4ab73709d88d08bfc5e02f1d341
mohitj8790 <mohitj8790@gmail.com>
2020-06-11 19:52:00 +0530

    Unique Words

--- Week2/wordFrequencies.java
+++ Week2/wordFrequencies.java
@@ -1,11 +1,40 @@
 package Week2;
+import java.util.*;
+import edu.duke.*;
 
 public class wordFrequencies 
 {
+    private ArrayList<String> myWords;
+
+    public wordFrequencies()
+    {
+        myWords=new ArrayList<String>();
+    }
+
+    public void findUnique() 
+    {
+        FileResource fr=new FileResource();
+        for(String word:fr.words())
+        {
+            word=word.toLowerCase();
+            int index=myWords.indexOf(word);
+            if(index==-1)
+            {
+                myWords.add(word);
+            }
+        }
+    }
+
+    public void tester() 
+    {
+        findUnique();
+        System.out.println("#Unique Words: "+myWords.size());
+    }
 
     public static void main(String[] args) 
     {
-        
+        wordFrequencies wf=new wordFrequencies();
+        wf.tester();
     }
     
 }
