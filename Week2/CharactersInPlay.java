88fae06d635593e1ba6ef334c2f9d96b77b9176f
mohitj8790 <mohitj8790@gmail.com>
2020-06-12 01:31:08 +0530

    character in play

---
+++ Week2/CharactersInPlay.java
@@ -0,0 +1,11 @@
+package Week2;
+import java.util.*;
+
+public class CharactersInPlay 
+{
+    public static void main(String[] args) 
+    {
+        CharactersInPlay cp=new CharactersInPlay();
+        
+    }
+}
