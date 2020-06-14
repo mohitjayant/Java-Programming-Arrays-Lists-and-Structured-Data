8448155f2eec528c1302a68330f522dcbfc33a6a
mohitj8790 <mohitj8790@gmail.com>
2020-06-14 23:59:53 +0530

    make Story

--- Week2/GladLib/gladLib.java
+++ Week2/GladLib/gladLib.java
@@ -3,9 +3,22 @@ package Week2.GladLib;
 public class gladLib 
 {
 
+    private void printOut(String story,int lineWidth)
+    {
+
+    }
+
+    public String fromTemplate(String source)
+    {
+        String story="";
+        return story;
+    }
+
     public void makeStory() 
     {
-        
+        System.out.println("\n");
+        String story=fromTemplate("data/madTemplate.txt");
+        printOut(story,60);
     }
 
     public static void main(String[] args) 
