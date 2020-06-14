0973463835a2d2bf24f6fcdf07694142d48aff9f
mohitj8790 <mohitj8790@gmail.com>
2020-06-14 23:53:20 +0530

    make Story

--- Week2/GladLib/gladLib.java
+++ Week2/GladLib/gladLib.java
@@ -1,5 +1,16 @@
 package Week2.GladLib;
 
-public class gladLib {
-    
+public class gladLib 
+{
+
+    public void makeStory() 
+    {
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        gladLib gl=new gladLib();
+        gl.makeStory();
+    }
 }
